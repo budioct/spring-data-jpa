1c79d8c690b7cd0fa06f9b7c8234d6b59d276c4e
Budhi Octaviansyah <budioct@gmail.com>
2023-07-01 19:03:34 +0700

    Example dan ExampleMatcher object entity yang di reflection

--- src/test/java/com/tutorial/query/QueryMethodTest.java
+++ src/test/java/com/tutorial/query/QueryMethodTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 
 import java.util.List;
 
@@ -80,5 +82,84 @@ public class QueryMethodTest {
           */
 
     }
+
+    /**
+     * Example
+     * ● Spring Data JPA memiliki fitur Query by Example, dimana kita bisa membuat data object Entity, lalu
+     *   meminta Spring Data JPA untuk membuat Query berdasarkan data example Entity yang kita buat
+     * ● https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/domain/Example.html
+     *
+     * Example Repository
+     * ● JpaRepository memiliki parent interface QueryByExampleExecutor
+     * ● Dimana sudah disediakan banyak method yang bisa kita gunakan dengan parameter Example untuk mencari data
+     * ● https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/query/QueryByExampleExecutor.html
+     */
+
+    @Test
+    void testQueryByExample(){
+
+        Category category = new Category();
+        category.setName("BUKU");
+
+        Example<Category> example = Example.of(category); // interface Example<T> // static <T> Example<T> of(T probe) // buat yang baru Example termasuk semua propertry non-null secara default
+
+        List<Category> categories = categoryRepository.findAll(example); // <S extends T> List<S> findAll(Example<S> example)
+        Assertions.assertEquals(1, categories.size());
+
+        /**
+         * result query:
+         * Hibernate:
+         *     select
+         *         c1_0.id,
+         *         c1_0.created_date,
+         *         c1_0.last_modified_date,
+         *         c1_0.name
+         *     from
+         *         categories c1_0
+         *     where
+         *         c1_0.name=?
+         */
+
+    }
+
+    /**
+     * Example Matcher
+     * ● Example memiliki fitur Matcher, dimana kita bisa atur cara Example melakukan query
+     * ● https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/domain/ExampleMatcher.html
+     */
+
+    @Test
+    void testQueryByExampleMatcher(){
+
+        Category category = new Category();
+        category.setName("alat tulis");
+
+        // interface ExampleMatcher
+        // static ExampleMatcher matching() // akan di cocokan dengan query reflection object
+        // ExampleMatcher withIgnoreCase() // akan mengabaikan huruf besar kecilnya yang ada di kolom
+        // ExampleMatcher withIgnoreNullValues() // akan mengabaikan apakah null atau tidak null
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withIgnoreNullValues();
+
+        Example<Category> example = Example.of(category, matcher); // static <T> Example<T> of(T probe, ExampleMatcher matcher) // buat yang baru Example termasuk semua propertry non-null secara default
+
+        List<Category> categories = categoryRepository.findAll(example); // <S extends T> List<S> findAll(Example<S> example)
+        Assertions.assertEquals(1, categories.size());
+
+        /**
+         * result query:
+         * Hibernate:
+         *     select
+         *         c1_0.id,
+         *         c1_0.created_date,
+         *         c1_0.last_modified_date,
+         *         c1_0.name
+         *     from
+         *         categories c1_0
+         *     where
+         *         lower(c1_0.name)=?
+         */
+
+    }
+
 
 }
